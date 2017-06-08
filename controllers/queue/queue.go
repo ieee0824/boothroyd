3b03407e2759aef5a6aa37edde47adc2c61cbd52
ieee0824 <ieee0824@ezweb.ne.jp>
2017-06-08 15:45:20 +0900

    queueを改良

--- controllers/queue/queue.go
+++ controllers/queue/queue.go
@@ -1,11 +1,8 @@
 package queue
 
 import (
-	"net/url"
 	"github.com/jobtalk/hawkeye/models/queue"
-	"math/rand"
 	"time"
-	"github.com/pkg/errors"
 	"gopkg.in/kataras/iris.v6"
 	"github.com/jobtalk/hawkeye/models/work"
 	"encoding/json"
@@ -13,15 +10,12 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/aws/aws-sdk-go/aws"
+	"net/url"
 )
 
-func init(){
-	rand.Seed(time.Now().UnixNano())
-}
+var qs = queue.New()
 
 var hosts = []string{}
-
-type queues map[string]*queue.Queue
 
 func execLambdaFunction(param []byte)(string, error){
 	sess := session.New(&aws.Config{Region: aws.String("ap-northeast-1")})
@@ -34,67 +28,29 @@ func execLambdaFunction(param []byte)(string, error){
 	return result.GoString(), err
 }
 
-func shuffle(data []string) {
-	n := len(data)
-	for i := n - 1; i >= 0; i-- {
-		j := rand.Intn(i + 1)
-		data[i], data[j] = data[j], data[i]
-	}
-}
-
-func (q *queues) Enqueue(s string) error {
-	w := &work.Work{}
-	if err := json.Unmarshal([]byte(s), w); err != nil {
-		return err
-	}
-	u, err := url.Parse(w.URL)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	if _, ok := (*q)[u.Host]; !ok {
-		hosts = append(hosts, u.Host)
-		(*q)[u.Host] = queue.New()
-	}
-	(*q)[u.Host].Enqueue(s)
-	return nil
-}
-
-func (q *queues) Dequeue()(string, error) {
-	keys := make([]string, len(hosts))
-	copy(keys, hosts)
-	shuffle(keys)
-
-	for _, key := range keys {
-		ret, err := (*q)[key].Dequeue()
-		if err == nil {
-			return ret, nil
-		}
-	}
-	return "", errors.New("There is no data that can be retrieved")
-}
-
-var qs = &queues{}
-
-
-
 func Run() {
 	for {
-		target, err := qs.Dequeue()
-		if err != nil {
-			continue
+		select {
+		case d := <- qs.C:
+			var target []byte
+			if _, ok := d.([]byte); ok {
+				target = d.([]byte)
+			} else if _, ok := d.(string); ok {
+				target = []byte(d.(string))
+			} else {
+				continue
+			}
+
+			result, err := execLambdaFunction(target)
+			fmt.Println(result, err)
 		}
-		fmt.Println(target)
-		// lambda functionを呼び出す
-		result, err := execLambdaFunction([]byte(target))
-		fmt.Println(result, err)
 	}
 }
 
 func Check(ctx *iris.Context) {
 	ctx.JSON(
 		200,
-		qs,
+		qs.Status(),
 	)
 }
 
@@ -110,6 +66,18 @@ func Enqueue(ctx *iris.Context) {
 		)
 		return
 	}
+	u, err := url.Parse(w.URL)
+	if err != nil {
+		ctx.JSON(
+			500,
+			map[string]interface{}{
+				"status": false,
+				"error": err,
+			},
+		)
+		return
+	}
+
 	w.JobID = fmt.Sprint(time.Now().UnixNano())
 	if bin, err := json.Marshal(w); err != nil {
 		ctx.JSON(
@@ -121,7 +89,7 @@ func Enqueue(ctx *iris.Context) {
 		)
 		return
 	} else {
-		qs.Enqueue(string(bin))
+		qs.Enqueue(u.Host, bin)
 	}
 
 	ctx.JSON(
