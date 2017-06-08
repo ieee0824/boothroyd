b113a10b19590f726976e50eee5f55496aec4506
ieee0824 <ieee0824@ezweb.ne.jp>
2017-06-08 15:45:20 +0900

    queueを改良

--- models/queue/queue.go
+++ models/queue/queue.go
@@ -2,43 +2,86 @@ package queue
 
 import (
 	"time"
-	"github.com/pkg/errors"
 )
 
+const (
+	MAX_QUEUE_SIZE = 1 *1024 * 1024
+)
+
+type innerQueue struct {
+	data chan interface{}
+	status int
+	c chan interface{}
+	delay time.Duration
+	lastDeque time.Time
+}
+
+func newInnnerQueue() *innerQueue {
+	return &innerQueue {
+		make(chan interface{}, MAX_QUEUE_SIZE),
+		0,
+		make(chan interface{}),
+		5 * time.Second,
+		time.Now().AddDate(-1,0,0),
+	}
+}
+
+func (q *innerQueue) enqueue(i interface{}) {
+	q.data <- i
+	q.status ++
+}
+
+func (q *innerQueue) dequeue() chan interface{} {
+	go func() {
+		for {
+			d := <- q.data
+
+			if time.Now().Sub(q.lastDeque) < q.delay {
+				for time.Now().Sub(q.lastDeque) < q.delay {
+					time.Sleep(q.delay - time.Now().Sub(q.lastDeque))
+				}
+			}
+			q.c <- d
+			q.status --
+			q.lastDeque = time.Now()
+		}
+	}()
+	return q.c
+}
+
 type Queue struct {
-	LastPopedAt *time.Time
-	Weight time.Duration
-	Data []string
+	C chan interface{}
+	data map[string]*innerQueue
 }
 
-func New()*Queue{
+func New()*Queue {
 	return &Queue{
-		nil,
-		10 * time.Second,
-		make([]string, 0, 1024),
+		make(chan interface{}),
+		map[string]*innerQueue{},
 	}
 }
 
-func (q *Queue) Enqueue (dataSet ...string) {
-	q.Data = append(q.Data, dataSet...)
+func (q *Queue) Enqueue(key string, i interface{}) {
+	if _, ok := q.data[key]; !ok {
+		q.data[key] = newInnnerQueue()
+
+	}
+	q.data[key].enqueue(i)
+	go func() {
+		for {
+			select {
+			case d := <- q.data[key].dequeue():
+				q.C <- d
+			}
+		}
+	}()
 }
 
-func (q *Queue) Dequeue () (string, error) {
-	if q == nil {
-		return "", errors.New("queue is nil")
+func (q *Queue) Status() map[string]interface{} {
+	var r = map[string]interface{}{}
+	for k, v := range q.data {
+		r[k] = v.status
+		r[k] = v.status
 	}
-	if len(q.Data) == 0 {
-		return "", errors.New("queue is empty")
-	}
-
-	if q.LastPopedAt != nil && (time.Now().Sub(*q.LastPopedAt) < q.Weight) {
-		return "", errors.New("Dequeue Time interval too short")
-	}
-
-	ret := q.Data[0]
-	q.Data = q.Data[1:]
-	now := time.Now()
-	q.LastPopedAt = &now
-	return ret, nil
+	return r
 }
-
